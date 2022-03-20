86ea47c792c44c435ffc8a1a54e86cc2a7265210
EC2 Default User <ec2-user@ip-172-31-26-18.eu-west-2.compute.internal>
2022-03-20 20:34:46 +0000

    modified

--- SS/ss.tf
+++ SS/ss.tf
@@ -31,12 +31,12 @@ resource "azurerm_resource_group" "terraform" {
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.terraform.name
-  virtual_network_name = azurerm_virtual_network.terraform.name
+  virtual_network_name = "terraform_vnet"
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "terraform" {
-  name                = "terraform-vmss"
+  name                = "terraform_vmss"
   resource_group_name = azurerm_resource_group.terraform.name
   location            = azurerm_resource_group.terraform.location
   sku                 = "Standard_F2"
