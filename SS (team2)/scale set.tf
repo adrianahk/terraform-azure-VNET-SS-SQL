1369d5645b3f92fd2f3d99e8a8aa6824ca327599
EC2 Default User <ec2-user@ip-172-31-26-18.eu-west-2.compute.internal>
2022-03-20 18:53:16 +0000

    added SS folder and ss file

--- SS (team2)/scale set.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_linux_virtual_machine_scale_set" "example" {
-  name                = "example-vmss"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  sku                 = "Standard_F2"
-  instances           = 1
-
-}
