e0a535b23c91ade23bb6352da30bf8c149cf5c35
Brochek <96146278+Brochek@users.noreply.github.com>
2022-03-29 18:09:42 -0500

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,26 @@
+output "vnet_id" {
+    value = azurerm_virtual_network.terraform.id
+}
+
+
+output "subnet"{
+    value = azurerm_virtual_network.terraform.subnet
+}
+
+output "subnet_id" {
+  value = azurerm_virtual_network.terraform.id
+}
+output "vnet_name"{
+    value = azurerm_virtual_network.terraform.name
+}
+output resource_group_name {
+  value = azurerm_resource_group.terraform.name
+}
+
+output resource_group_location {
+   value = azurerm_resource_group.terraform.location
+}
+
+output "full_info" {
+ value = data.terraform_remote_state.main.outputs.*
+}
