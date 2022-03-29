3d10e00b1f85de06e7026bef29fba35ae0e5a060
AdrianaHk <93105450+AdrianaHk@users.noreply.github.com>
2022-03-29 11:54:57 -0500

    Update ss.tf

--- SS/ss.tf
+++ SS/ss.tf
@@ -52,7 +52,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "terraform_ss" {
     ip_configuration {
       name      = "internal"
       primary   = true
-      subnet_id = "/subscriptions/9fa90ac8-c1ee-4759-ba48-216e13b0c938/resourceGroups/terraform-resources/providers/Microsoft.Network/virtualNetworks/terraform_vnet"
+      subnet_id =  "/subscriptions/9fa90ac8-c1ee-4759-ba48-216e13b0c938/resourceGroups/terraform-resources/providers/Microsoft.Network/virtualNetworks/terraform_vnet"
     }
   }
 }
@@ -87,7 +87,7 @@ resource "azurerm_traffic_manager_profile" "tm-profile" {
   }
 
 resource "azurerm_traffic_manager_azure_endpoint" "terraform" {
-  target_resource_id  = azurerm_public_ip.ip.id
+  target_resource_id  = "/subscriptions/9fa90ac8-c1ee-4759-ba48-216e13b0c938/resourceGroups/terraform-resources/providers/Microsoft.DBforMySQL/servers/project-mysqlserver/databases/projectdb"
   name                = "terraform-endpoint"
   profile_id          = azurerm_traffic_manager_profile.tm-profile.id
   weight              = 100
