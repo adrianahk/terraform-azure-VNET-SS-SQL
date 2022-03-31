45f2b2cb7453805de0097af7a7f9f4d1d247b3cf
Farrukh Sadykov <farrukhsadykov@gmail.com>
2022-03-30 19:28:58 -0500

    updated subnet id

--- VNET/vnet.tf
+++ VNET/vnet.tf
@@ -83,7 +83,8 @@ output "subnet"{
 }
 
 output "subnet_id" {
-  value = azurerm_virtual_network.terraform.id
+  value = "${azurerm_virtual_network.terraform.subnet.*.id[1]}"
+
 }
 output "vnet_name"{
     value = azurerm_virtual_network.terraform.name
