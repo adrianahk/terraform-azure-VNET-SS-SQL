2f9a347d60d2768563aeede413e881001ec80d16
AdrianaHk <93105450+AdrianaHk@users.noreply.github.com>
2022-03-31 16:25:28 -0500

    Update ss.tf

--- SS/ss.tf
+++ SS/ss.tf
@@ -97,7 +97,7 @@ resource "azurerm_traffic_manager_profile" "tm-profile" {
   }
 
   resource "azurerm_traffic_manager_azure_endpoint" "terraform" {
-  target_resource_id  = data.terraform_remote_state.team3.output.recource_id
+  target_resource_id  = data.terraform_remote_state.team3.outputs.recource_id
   name                = "terraform-endpoint"
   profile_id          = azurerm_traffic_manager_profile.tm-profile.id
   weight              = 100
